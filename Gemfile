15edf722b3280788642926bcac30c35d5ea6c384
Sufyan Adam <pair+sufyanadam@gmail.com>
2017-01-14 15:31:44 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in hl7tocsv.gemspec
+gemspec
---
+++ spec/hl7tocsv_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+describe HL7toCSV do
+  it "has a version number" do
+    expect(HL7toCSV::VERSION).not_to be nil
+  end
+
+  describe '.convert' do
+    subject { HL7toCSV.convert(hl7) }
+    let(:hl7) { File.read('spec/fixtures/sample.hl7') }
+
+    it "works" do
+      expect(subject).to eq "ID,GLUCOSE GLC BASELINE\n845439^GHH OE,^182\n"
+    end
+  end
+end
